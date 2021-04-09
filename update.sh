5ff2309eff6ea1f8fe3c703033e241054589531f
Oscar Veldman <oveldman@gmail.com>
2021-04-09 15:33:39 +0200

    Update the update script

--- update.sh
+++ update.sh
@@ -1,16 +1,20 @@
 #!/bash/sh
 #Get latest sources and updates
+echo "Start script."
 apt update
 git pull
 
 #update API
+echo "Start deploying dotnet API."
 systemctl stop kestrel-madworldapi.service
 cd API/Mad-World.API
 dotnet restore
 dotnet publish --configuration Release --output ../../../Published/MadWorld/API
 systemctl start kestrel-madworldapi.service
+echo "Dotnet API is deployed."
 
 #update Website
+echo "Start deploying angular website."
 cd ../../Website
 npm install
 ng update
@@ -18,3 +22,5 @@ npm update
 ng build --prod --output-path ../../../Published/MadWorld/Website
 rm -r /var/www/html/*
 mv ../../../Published/MadWorld/Website/* /var/www/html
+echo "Angular website is deployed."
+echo "The script is finsihed!"
