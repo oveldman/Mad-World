67bf5ea2bb9882a2a4ccf61b72e0fbb487812b20
Oscar Veldman <oveldman@gmail.com>
2021-04-15 20:00:30 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -25,3 +25,14 @@ snap refresh core
 snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
 #certbot --nginx (Only when everything is ready!)
+
+# Docker
+apt-get update
+apt-get install apt-transport-https ca-certificates 
+apt-get install curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
