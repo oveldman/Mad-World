9c93b77ac160de2ddb08674503a8077efd05cec3
Oscar Veldman <oveldman@gmail.com>
2021-04-15 20:02:40 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -36,3 +36,4 @@ echo \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install docker-ce docker-ce-cli containerd.io
+docker pull postgres
