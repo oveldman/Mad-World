57e17881ca3bf4fb6672b8353885252802693a84
Oscar Veldman <oveldman@gmail.com>
2021-04-23 21:03:21 +0200

    Complete login system

--- Website/src/app/app.module.ts
+++ Website/src/app/app.module.ts
@@ -1,6 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +32,9 @@ import { LoginComponent } from './pages/authentication/login/login.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Website/src/app/pages/authentication/login/login.component.ts
+++ Website/src/app/pages/authentication/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl, Validators, Form } from '@angular/forms';
+import { AuthenticationService } from './../../../services/api/authentication/authentication.service'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public showError : boolean = false;
+  public IsLoggedIn : boolean = false;
+  public IsNotLoggedIn : boolean = true;
+  form:FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private authenticationService: AuthenticationService, private router: Router) { 
+    this.form = this.formBuilder.group({
+      username: ['',Validators.required],
+      password: ['',Validators.required]
+    });
+   }
 
   ngOnInit(): void {
+    this.checkIsLogin();
+  }
+
+  login(customerData: any) {
+    if (customerData.username && customerData.password) {
+      this.authenticationService.login(customerData.username, customerData.password).subscribe(bearer => {
+        this.showError = !bearer.Succes;
+
+        if (bearer.Succes) {
+          this.checkIsLogin();
+        } 
+      });
+    }
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigateByUrl('/');  
+  }
+
+  private checkIsLogin() {
+    const idToken = localStorage.getItem("id_token");
+    this.IsLoggedIn = idToken !== null;
+    this.IsNotLoggedIn = !this.IsLoggedIn;
   }
 
 }
